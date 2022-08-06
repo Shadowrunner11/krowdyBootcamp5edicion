d4b5a6f00ee79adf9155f5c4cc1d52d309b0ac31
Shadowrunner11 <matematicadivina1@gmail.com>
2022-08-06 17:21:26 -0500

    [UPD] scrapper config added

---
+++ src/service/fetchService.js
@@ -0,0 +1,15 @@
+class FetchService {
+  urlApi = 'http://localhost:3000/profiles';
+
+  async createUrlProfiles(urlsCandidates) {
+    return fetch(this.urlApi ,{ 
+      method: 'POST',
+      body:JSON.stringify({ urlsCandidates }),
+      headers:{
+        'Content-type': 'application/json; charset=UTF-8'
+      } 
+    } );
+  }
+}
+
+export default new FetchService();
