06803e74f441af171d9b5865c2b7ff8ccfa73d4a
Shadowrunner11 <matematicadivina1@gmail.com>
2022-08-06 08:41:56 -0500

    [UPD] added axios service and pagination logic

--- config/esbuild.js
+++ config/esbuild.js
@@ -1,18 +1,24 @@
 import esbuild from 'esbuild';
+import { envs } from '../.env';
+
 const entryPoints = [
   'src/sw.js','src/scripts/scrapper.js',
   'src/scripts/pop.js',
-  'src/scripts/scrapCandidates.js'
+  'src/scripts/scrapCandidates.js',
+  'src/scripts/scrapperV2'
 ];
+
+const {MINIFIED, WATCH} = envs;
 
 esbuild.build({
   entryPoints,
-  watch: true,
+  watch: WATCH,
   bundle: true,
   outdir: 'dist',
   // target: 'chrome',
-  // minify: true,
-  allowOverwrite: true
+  minify: MINIFIED,
+  allowOverwrite: true,
+  inject:['dev.env.js']
 })
   .then(response => console.log(JSON.stringify(response)))
   .catch(err => console.log(err));
